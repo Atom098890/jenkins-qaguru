f93b8d050c53dce93656d3ae1bf47ef6dc75afce
Andrey <atom.098890@gmail.com>
2022-11-08 22:59:40 +0300

    add screenshots

--- src/test/java/tests/demoqa/regitsterForm/tests/TestBase.java
+++ src/test/java/tests/demoqa/regitsterForm/tests/TestBase.java
@@ -2,7 +2,9 @@ package tests.demoqa.regitsterForm.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 
@@ -14,5 +16,12 @@ public class TestBase {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+    @AfterEach
+    void addAttach() {
+        Attach.screenShot("Last screenshots");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
     }
 }
