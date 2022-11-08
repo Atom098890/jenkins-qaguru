d92f48f038acc35779dadd2e414f4c4a00381619
Andrey <atom.098890@gmail.com>
2022-11-08 22:44:01 +0300

    add selenoid

--- src/test/java/tests/demoqa/regitsterForm/tests/TestBase.java
+++ src/test/java/tests/demoqa/regitsterForm/tests/TestBase.java
@@ -1,13 +1,18 @@
 package tests.demoqa.regitsterForm.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 
 
 
 public class TestBase {
+
     @BeforeAll
     static void config() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 }
